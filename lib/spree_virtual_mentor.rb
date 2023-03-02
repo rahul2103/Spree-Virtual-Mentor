e347a6c37f6006e27628ebb8a70c08728fa7658b
rahul2103 <rahulsingh2103@yahoo.com>
2023-03-02 23:51:10 +0530

    first commit

---
+++ lib/spree_virtual_mentor.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_virtual_mentor/engine'
+require 'spree_virtual_mentor/version'
---
+++ lib/spree_virtual_mentor/engine.rb
@@ -0,0 +1,24 @@
+module SpreeVirtualMentor
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_virtual_mentor'
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    initializer 'spree_virtual_mentor.environment', before: :load_config_initializers do |_app|
+      SpreeVirtualMentor::Config = SpreeVirtualMentor::Configuration.new
+    end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare(&method(:activate).to_proc)
+  end
+end
