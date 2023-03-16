0f25c64d2116065d1c4d9e226f4705c8c921aae2
rahul2103 <rahulsingh2103@yahoo.com>
2023-03-16 15:43:03 +0530

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   namespace :admin do
     resource :mentors, only: [] do
+      get 'chat'
       get 'fine-tunes-lists'
       post 'create-fine-tunes'
       put 'set-completions-model'
